877068c19de711f1bfdaf5946654693545e8547c
branderghn <83637085+branderghn@users.noreply.github.com>
2023-04-30 10:25:32 -0400

    updated time on script

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-function displayTemperature(response){
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = date.getDay();
+    return `${day} ${hours} ${minutes}`;
+}
+
+function displayTemperature(response) {
     console.log(response.data.main.temp);
     let cityElement = document.querySelector("#city");
     let description = document.querySelector("#description");
     let temperatureElement = document.querySelector("#temperature");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
     cityElement.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     windElement.innerHTML = response.data.main.wind.speed;
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
